a6a6edb86523dc5b06b57cb9989d50725e4b61b1
fgandraf <fgandraf@gmail.com>
2023-06-22 00:25:57 -0300

    Screenshots path and download resume section

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});

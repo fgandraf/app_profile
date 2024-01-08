6fd437475e201b41068a766aebb25c1b86963055
fgandraf <fgandraf@gmail.com>
2024-01-08 14:09:24 -0300

    feat: revisao de layout

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,20 +1,20 @@
-function openModal(path) 
-{
+function openModal(path) {
     var modal = document.getElementById("myModal");
     document.getElementById("modalContent").innerHTML = '<object type="text/html" data="' + path + '"></object>';
     modal.style.display = "block";
+
+    document.body.style.overflow = "hidden";
 }
 
-function closeModal() 
-{
+function closeModal() {
     var modal = document.getElementById("myModal");
     modal.style.display = "none";
+    document.body.style.overflow = "";
 }
 
 
 // Quando o usuário clicar no botão de fechar, fecha a janela modal
 var a = document.getElementsByClassName("close")[0];
-a.onclick = function()
-{
+a.onclick = function () {
     closeModal();
 }

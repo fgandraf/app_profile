cc872b9b112b2370801daad2cb46c2f3c9e6ad32
fgandraf <fgandraf@gmail.com>
2024-01-08 21:30:44 -0300

    feat: Alucar cover and description fix: purge v1 folder

--- v1/js/swiper.js
+++
@@ -1,8 +0,0 @@
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 1,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});

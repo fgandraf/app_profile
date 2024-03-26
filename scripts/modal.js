d21efc21b6a6d2732f024ef19b75e0598fa1e83e
fgandraf <fgandraf@gmail.com>
2024-03-26 00:18:47 -0300

    feat: small fixes and cards layout

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,25 @@
-function openModal(path) {
+function openModal(path, size) {
     var modal = document.getElementById("myModal");
-    document.getElementById("modalContent").innerHTML = '<object type="text/html" data="' + path + '"></object>';
-    modal.style.display = "block";
+    // Cria um novo elemento object
+    var objectElement = document.createElement("object");
+    objectElement.type = "text/html";
+    objectElement.data = path;
 
+    // Verifica se size é "full" e aplica os estilos
+    if (size === "full") {
+        objectElement.style.width = "90vw";
+        objectElement.style.height = "80vh";
+    }
+
+    // Limpa o conteúdo anterior e adiciona o novo elemento object
+    var modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = ""; // Limpa qualquer conteúdo anterior
+    modalContent.appendChild(objectElement); // Adiciona o novo elemento object
+
+    modal.style.display = "block";
     document.body.style.overflow = "hidden";
 }
+
 
 function closeModal() {
     var modal = document.getElementById("myModal");

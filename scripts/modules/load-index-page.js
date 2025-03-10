15feb9a67e9f58956549aaa579526944d154eef8
Felipe Gandra <fgandraf@gmail.com>
2025-03-10 14:31:21 -0300

    refactor certificates and technologies

--- scripts/modules/load-index-page.js
+++ scripts/modules/load-index-page.js
@@ -1,14 +1,8 @@
 export function loadIndexPage(lang, flag) {
-    // Flag Selector
-    document.getElementById("flag-br").style.borderBottom = 'solid 2px transparent';
-    document.getElementById("flag-en").style.borderBottom = 'solid 2px transparent';
-    document.getElementById("flag-es").style.borderBottom = 'solid 2px transparent';
-    document.getElementById(`flag-${flag}`).style.borderBottom = 'solid 2px white';
     
     // AboutSection
     document.getElementById("about-title").innerText = lang.about.title;
-    const topics = document.getElementById("about-topics");
-    topics.innerHTML = lang.about.topics.map(topic => `<li>${topic}</li>`).join('');
+    document.getElementById("about-topics").innerHTML = lang.about.topics.map(topic => `<li>${topic}</li>`).join('');
     document.getElementById("about-more").innerText = lang.about.moreButton;
 
     // TechnologiesSection
@@ -17,41 +11,53 @@ export function loadIndexPage(lang, flag) {
     // CertificatesSection
     document.getElementById("certificates-title").innerText = lang.certificates.title;
 
-    const certificate1 = document.getElementById("certificate1");
-    certificate1.querySelector('.title').innerText = lang.certificates.certificate1.title;
-    certificate1.querySelector('.right > .institution').innerText = lang.certificates.certificate1.institution;
-    certificate1.querySelector('.right > .date').innerText = lang.certificates.certificate1.date;
 
-    const certificate2 = document.getElementById("certificate2");
-    certificate2.querySelector('.title').innerText = lang.certificates.certificate2.title;
-    certificate2.querySelector('.right > .institution').innerText = lang.certificates.certificate2.institution;
-    certificate2.querySelector('.right > .date').innerText = lang.certificates.certificate2.date;
+    //academic
+    document.querySelector(".academic .content .title").innerHTML = lang.certificates.academic.title;
+    document.querySelector(".academic .content .subtitle").innerHTML = lang.certificates.academic.subtitle;
+    document.querySelector(".academic .content .institution").innerHTML = lang.certificates.academic.institution;
+    document.querySelector(".academic .content .year").innerHTML = lang.certificates.academic.year;
 
-    const certificate3 = document.getElementById("certificate3");
-    certificate3.querySelector('.title').innerText = lang.certificates.certificate3.title;
-    certificate3.querySelector('.right > .institution').innerText = lang.certificates.certificate3.institution;
-    certificate3.querySelector('.right > .date').innerText = lang.certificates.certificate3.date;
-    
-    const certificate4 = document.getElementById("certificate4");
-    certificate4.querySelector('.title').innerText = lang.certificates.certificate4.title;
-    certificate4.querySelector('.right > .institution').innerText = lang.certificates.certificate4.institution;
-    certificate4.querySelector('.right > .date').innerText = lang.certificates.certificate4.date;
-    
-    
-    document.getElementById('todos_os_cursos').innerText = lang.certificates.courses;
-    
+
+    //certificate Fiap
+    document.querySelector("#certificate_fiap .title").innerHTML = lang.certificates.certificateFiap.title;
+    document.querySelector("#certificate_fiap .content .certificate-item ul").innerHTML = `
+        <li>${lang.certificates.certificateFiap.first.title}</li>
+        <span class="date">${lang.certificates.certificateFiap.first.date}</span>
+        <li>${lang.certificates.certificateFiap.second.title}</li>
+        <span class="date">${lang.certificates.certificateFiap.second.date}</span>
+        <li>${lang.certificates.certificateFiap.third.title}</li>
+        <span class="date">${lang.certificates.certificateFiap.third.date}</span>
+        <li>${lang.certificates.certificateFiap.fourth.title}</li>
+        <span class="date">${lang.certificates.certificateFiap.fourth.date}</span>
+    `;
+
+    //others
+    document.querySelector("#certificate_others .title").innerHTML = lang.certificates.others.title;
+    document.querySelector("#certificate_others .content .certificate-more").innerHTML = lang.certificates.others.more;
+   
     // PortfolioSection
     document.getElementById("portfolio-title").innerText = lang.portfolio.title;
-    loadProject(lang, "money");
-    loadProject(lang, "promoreApi");
-    loadProject(lang, "ethor");
-    loadProject(lang, "profile");
-    loadProject(lang, "maar");
-    loadProject(lang, "vagalivre");
-    loadProject(lang, "alucar");
-    loadProject(lang, "fluxusApi");
-    loadProject(lang, "fluxusApp");
-    loadProject(lang, "aex30");
+
+    lang.portfolio.projects.forEach(el => {
+        const projectName = Object.keys(el)[0];
+        const project = lang.portfolio.projects.find(element => element[projectName])[projectName];
+
+        // Carregar elementos do DOM
+        let tag = document.querySelector(`#${projectName} > .card > .card__tag`)
+        let stacks = document.querySelector(`#${projectName} > .card > .card__content > .card__stacks`);
+        let name = document.querySelector(`#${projectName} > .card > .card__content > .projeto__nome`);
+        let description = document.querySelector(`#${projectName} > .card > .card__content > .projeto__descricao`);
+        let lastUpdate = document.querySelector(`#${projectName} > .card > .card__content > .last_update`);
+
+        // // Atualizar valores
+        tag.innerText = project.tag;
+        stacks.innerText = "";
+        project.stacks.forEach(element => { stacks.innerHTML += `<li class="stack__item">${element}</li>`});
+        name.innerText = project.name;
+        description.innerText = project.shortDescription;
+        lastUpdate.innerText = project.lastUpdate;
+    });
 
     // Footer
     // ---resume
@@ -74,32 +80,4 @@ export function loadIndexPage(lang, flag) {
     document.getElementById("desktop-footer__contacts-phone").innerText = lang.footer.contacts.phone;
     document.getElementById("desktop-footer__contacts-github").innerText = lang.footer.contacts.github;
     document.getElementById("desktop-footer__contacts-linkedin").innerText = lang.footer.contacts.linkedin;
-}
-
-
-function loadProject(lang, projectName) {
-
-    // Carregar informações do json
-    const project = lang.portfolio.projects.find(element => element[projectName])[projectName];
-
-    // Carregar elementos do DOM
-    let tag = document.querySelector(`#${projectName} > .card > .card__tag`);
-    let stacks = document.querySelector(`#${projectName} > .card > .card__content > .card__stacks`);
-    let name = document.querySelector(`#${projectName} > .card > .card__content > .projeto__nome`);
-    let description = document.querySelector(`#${projectName} > .card > .card__content > .projeto__descricao`);
-    let lastUpdate = document.querySelector(`#${projectName} > .card > .card__content > .last_update`);
-
-    // Limpar valores
-    tag.innerText = "";
-    stacks.innerText = "";
-    name.innerText = "";
-    description.innerText = "";
-    lastUpdate.innerText = "";
-
-    // Atualizar valores
-    tag.innerText = project.tag;
-    project.stacks.forEach(element => { stacks.innerHTML += `<li class="stack__item">${element}</li>`});
-    name.innerText = project.name;
-    description.innerText = project.shortDescription;
-    lastUpdate.innerText = project.lastUpdate;
 }
